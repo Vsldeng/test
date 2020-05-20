bdd588c6b7d63ccb66b960f1bb02b4a6b05e6852
v_sldeng <kmckkdeng@21kunpeng.com>
2020-05-20 18:13:21 +0800

    5.20

---
+++ Web/WEB/aliall/009react/demo-react/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app'
+
+import './index.css'
+//第三方模块写文件名就行，自定义模块必须写相对路径
+ReactDOM.render(<App />,document.getElementById('root'));
