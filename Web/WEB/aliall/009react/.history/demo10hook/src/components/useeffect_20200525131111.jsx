e3c33c00488f53736d6dbc59386a5796c7499477
v_sldeng <kmckkdeng@21kunpeng.com>
2020-08-03 15:16:35 +0800

    8.5

--- Web/WEB/aliall/009react/.history/demo10hook/src/components/useeffect_20200525131111.jsx
+++ Web/WEB/aliall/009react/.history/demo10hook/src/components/useeffect_20200525131111.jsx
@@ -8,7 +8,7 @@ const UseEffect =()=>{
         return ()=>{
             console.log('componentWillUnmount')//在useEffect里使用return是使用伪componentWillUnmount的钩子函数
         }
-    },[count])//第二个参数是控制监控哪个状态，这个状态（这些状态）改变，就会调用该伪钩子函数
+    },[count])//第二个参数是控制监控哪个状态，整个状态（这些状态）改变，就会调用该伪钩子函数
     return(
         <div>
             <h2>effect</h2>
